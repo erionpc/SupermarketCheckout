533bc1ae44dfdd5d66018247abe0762db13876d3
Erion Pici <erionpc@yahoo.it>
2020-10-03 23:55:40 +0100

    removed commented code

--- SupermarketCheckout/Checkout.Identity/Models/ApplicationUser.cs
+++ SupermarketCheckout/Checkout.Identity/Models/ApplicationUser.cs
@@ -16,26 +16,6 @@ namespace Checkout.Identity.Models
 
         public string SecurityStamp { get; set; }
 
-        //public DateTimeOffset? LockoutEnd { get; set; }
-
-        //public bool TwoFactorEnabled { get; set; }
-
-        //public bool PhoneNumberConfirmed { get; set; }
-
-        //public string PhoneNumber { get; set; }
-
-        //public string ConcurrencyStamp { get; set; }
-
-        //public bool EmailConfirmed { get; set; }
-
-        //public string NormalizedEmail { get; set; }
-
-        //public string NormalizedUserName { get; set; }
-
         public string Id { get; set; }
-
-        //public bool LockoutEnabled { get; set; }
-
-        //public int AccessFailedCount { get; set; }
     }
 }
